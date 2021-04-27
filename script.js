f12426012b6adf6da40dd59f4661dd1b44987d1d
linf644 <70988686+linf644@users.noreply.github.com>
2021-04-28 01:53:54 +0800

    finish js part

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 // define the input text box
 let inputElement = document.querySelector("#input");
+// define the output
+let answerElement = document.querySelector("#answerId");
 
 inputElement.addEventListener("keydown", function(event){
     console.log(inputElement.value);
 
-    if (event.key == "enter"){
+    if (event.key == "Enter"){
 
         let items = inputElement.value;
+        let result=" It's in ";
         // reset the text box with backspace
         inputElement.value = "";
 
-        if (items = "book"){
-            
-        }
+        if (items == "books"){
+            result+="bookshelf in the study";
+        }else if(items == "textbooks"){
+			result+="bookshelf in the twins’ rooms";
+		}else if(items == "headphones"){
+			result+="drawers in the study";
+		}else if(items == "passport"){
+			result+="drawer under the desk in the study";
+		}else if(items == "key"){
+			result+="basket in the sideboard";
+		}else if(items == "glasses"){
+			result+="white basket on the dresser";
+		}else if(items == "important files"){
+			result+="safe box";
+		}else if(items =="old photos"){
+			result+="storage room on the 3rd floor";
+		}
+		answerElement.innerHTML=result;
     }
 })
